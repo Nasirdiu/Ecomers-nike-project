695b6a9fdcf616089c486f205de36c79b4e943ea
nasirdiu <nasir.joypurhat@gmail.com>
2022-11-21 23:32:27 +0600

    data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Hero from './components/Hero';
-
+import {heroapi} from './data/data.js'
 const App = () => {
   return (
     <div>
       <main>
-        <Hero/>
+        <Hero heroapi={heroapi}/>
       </main>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const Hero = () => {
+const Hero = ({heroapi :{title,subtitle,img,btntext,videos,sociallinks}}) => {
+    // console.log(heroapi);
     return (
-        <div>
-            <div>
-                <div></div>
-                <div>
-                    <div>
-                        <h1></h1>
-                        <h1></h1>
-                        <button></button>
-                        <div></div>
-                        <div></div>
+        <div className=''>
+                <div className=''></div>
+                <div className=''>
+                    <div className=''>
+                        <h1>{title}</h1>
+                        <h1>{subtitle}</h1>
+                        <button>{btntext}</button>
+                        {/* <div></div>
+                        <div></div> */}
                     </div>
-                    <div>
-                        <img className='' src="" alt="" />
+                    <div className=''>
+                        <img className="" src={img} alt="" />
                     </div>
                 </div>
-            </div>
+           
         </div>
     );
 };
