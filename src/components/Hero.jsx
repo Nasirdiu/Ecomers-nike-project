acfa59f7d340fedac26db850d1f90f788c8c52b0
nasirdiu <nasir.joypurhat@gmail.com>
2022-11-22 20:30:34 +0600

    video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import Clips from './utils/Clips';
 
 const Hero = ({heroapi :{title,subtitle,img,btntext,videos,sociallinks}}) => {
     // console.log(heroapi);
     return (
-        <div className=''>
-                <div className=''></div>
-                <div className=''>
-                    <div className=''>
-                        <h1>{title}</h1>
-                        <h1>{subtitle}</h1>
-                        <button>{btntext}</button>
-                        {/* <div></div>
-                        <div></div> */}
+        <div className='relative h-auto w-auto flex flex-col'>
+                <div className='bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10'></div>
+                <div className='relative z-20 opacity-100 grid items-center justify-items-center nike-container'>
+                    <div className='grid items-center justify-items-center mt-28 md:mt-24'>
+                        <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>{title}</h1>
+                        <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>{subtitle}</h1>
+                        <button type='button' className='button-theme bg-slate-200 shadow-slate-200 rounded-xl my-5'>{btntext}</button>
+                        <div className='grid items-center gap-5 md:gap-3 absolute top-[33vh] lg:top-[27vh] left-[11%] xl:left-0 w-auto h-a'>
+                            {
+                                
+                                videos.map((val,i)=>(
+                                    <Clips 
+                                    key={i}
+                                    imgsrc={val.imgsrc}
+                                    clip={val.clip}
+                                    />
+                                ))
+                            }
+                        </div>
+                        <div className=''></div>
                     </div>
                     <div className=''>
-                        <img className="" src={img} alt="" />
+                        <img className="w-auto h-[45vh] lg:h-[35vh] md:h-[31vh] sm:h-[21vh] xsm-h[19vh] transitions-theme -rotate-[25deg] hover:rotate-0 cursor-pointer object-fill" src={img} alt="" />
                     </div>
                 </div>
            
