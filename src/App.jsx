21c402e3e9e310ca9bcd935cddb49793451a28a5
nasirdiu <nasir.joypurhat@gmail.com>
2022-11-23 22:18:55 +0600

    title

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Sales from './components/Sales';
-import {heroapi,popularsales} from './data/data.js'
+import {heroapi,popularsales,toprateslaes} from './data/data.js'
 const App = () => {
   return (
     <div>
       <main>
         <Hero heroapi={heroapi}/>
         <Sales endpoint={popularsales} ifExists />
+
+        <Sales endpoint={toprateslaes} />
+
       </main>
     </div>
   );
--- src/components/utils/Title.jsx
+++ src/components/utils/Title.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Title = () => {
+const Title = ({title}) => {
     return (
         <div>
-            <h1>Title</h1>
+            <h1>{title}</h1>
         </div>
     );
 };
